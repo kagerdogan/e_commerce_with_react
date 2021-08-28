67fadc43d768afedc797ae6b92e061c9129d66aa
unknown <kag.erdogan@gmail.com>
2021-08-28 17:57:02 +0300

    Create API connection and Create Shopping Cart

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
-const products =[
-    {id:1,name:'PC',description: 'Monster.',price:'50TL',image:'https://mcdn01.gittigidiyor.net/72160/721608068_0.jpg'},
-    {id:2,name:'Phone',description: 'Iphone X.',price:'500TL',image:'https://mcdn01.gittigidiyor.net/62952/tn50/629521404_tn50_0.jpg'}
-]
+/*const products=[
+    {id:1,name:'PC',description:'Monster',price:'500Tl',image:'https://mcdn01.gittigidiyor.net/72160/721608068_0.jpg'},
+    {id:1,name:'PHONE',description:'IPHONE',price:'50Tl',image:'https://mcdn01.gittigidiyor.net/62952/tn50/629521404_tn50_0.jpg'}
 
-const Products = () => {
+]*/
+
+const Products = ({products}) => {
+    const classes = useStyles();
     return (
-        <main>
-        <Grid container justify='center' spacing={4}>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
+        <Grid container justifyContent="center" spacing={4}>
             {products.map((product)=>(
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                     <Product product={product} />
